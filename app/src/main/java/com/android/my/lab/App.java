3bc682145e5d16edd65cf90d738d88c2542f3eea
zhengyuji <zhengyj@ttwz168.net>
2016-09-28 17:29:31 +0800

    [other] modify enviroment configure

---
+++ app/src/main/java/com/android/my/lab/App.java
@@ -0,0 +1,18 @@
+package com.android.my.lab;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by 郑裕集 on 16/9/20.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
